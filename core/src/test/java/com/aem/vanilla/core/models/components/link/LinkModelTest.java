5e37fc53e27b8de0b36ec3482046c37aec28ebf5
marco-guastalli-softtek-com <marco.guastalli@softtek.com>
2024-09-18 17:08:15 +0200

    add link component

---
+++ core/src/test/java/com/aem/vanilla/core/models/components/link/LinkModelTest.java
@@ -0,0 +1,45 @@
+package com.aem.vanilla.core.models.components.link;
+
+import com.aem.vanilla.core.testcontext.AppAemContext;
+import io.wcm.testing.mock.aem.junit5.AemContext;
+import io.wcm.testing.mock.aem.junit5.AemContextExtension;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.testing.mock.sling.ResourceResolverType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.notNullValue;
+
+@ExtendWith({MockitoExtension.class, AemContextExtension.class})
+class LinkModelTest {
+
+    AemContext aemContext = AppAemContext.newAemContext(ResourceResolverType.JCR_MOCK);
+
+    @BeforeEach
+    void init() {
+        aemContext.addModelsForClasses(LinkModel.class);
+        aemContext.load().json("/com/aem/vanilla/core/models/components/link/link.json", "/content");
+    }
+
+    @Test
+    void testModel() {
+        // given
+        final Resource resource = aemContext.resourceResolver().getResource("/content/aemvanilla/en/en/jcr:content/root/container/container/link");
+        assertThat(resource, notNullValue());
+        assertThat(resource.getResourceType(), is(LinkModel.RESOURCE_TYPE));
+        aemContext.currentResource(resource);
+        final LinkModel model = aemContext.request().adaptTo(LinkModel.class);
+        // then
+        assertThat(model, notNullValue());
+        assertThat(model.getClass().getName(), equalTo(LinkModel.class.getName()));
+        assertThat(model.getLinkURL(), is("/content/aemvanilla/language-masters/en"));
+        assertThat(model.getLinkText(), is("reload"));
+        assertThat(model.isEmpty(), is(Boolean.FALSE));
+    }
+
+}
